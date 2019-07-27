4405505abcfffd40144fff3cee6d6f7bc3b4fb36
Fernando <2480661+fernandreu@users.noreply.github.com>
2019-07-27 10:49:58 +0100

    Remove socket.io sample code

--- server/sockets.js
+++ server/sockets.js
@@ -35,14 +35,4 @@ export default function() {
     connectTerminal(ws, pid);
   });
 
-  // const messages = [];
-  // io.on('connection', (socket) => {
-  //   socket.on('last-messages', function (fn) {
-  //     fn(messages.slice(-50))
-  //   })
-  //   socket.on('send-message', function (message) {
-  //     messages.push(message)
-  //     socket.broadcast.emit('new-message', message)
-  //   })
-  // })
 }
